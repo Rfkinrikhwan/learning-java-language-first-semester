0d0522f55fc1be5be4783ffd5cc2eccb6c00ac39
Rfkinrikhwan <rifkinurikhwan9@gmail.com>
2025-04-08 18:29:01 +0700

    New Update

--- Semester 2/Modul_2/Latihan.java
+++ Semester 2/Modul_2/Latihan.java
@@ -4,18 +4,18 @@ public class FibonacciRekursif {
 
     // Fungsi rekursif untuk menghitung bilangan Fibonacci ke-n
     public static int fibonacci(int n) {
-        if (n == 0) return 0;          // base case 1
-        if (n == 1) return 1;          // base case 2
+        if (n == 0 || n == 1) return n;
+
         return fibonacci(n - 1) + fibonacci(n - 2); // rekursi
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // buat scanner untuk input
         System.out.print("Masukkan angka n untuk menghitung Fibonacci ke-n: ");
-        int n = scanner.nextInt(); // baca input dari user
+        int n = scanner.nextInt();
 
         System.out.println("Fibonacci ke-" + n + " adalah " + fibonacci(n));
 
-        scanner.close(); // tutup scanner (opsional, tapi baik dilakukan)
+        scanner.close();
     }
 }
